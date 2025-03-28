05ff7cc63f78850135f0d679e0e9337b4fd5cc26
Nico Jackson <nicolajack@gmail.com>
2025-03-28 18:21:40 -0400

    Uploaded WeSplit
    built a simple check-splitting ui to learn basics of swiftui

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by nico jackson on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
