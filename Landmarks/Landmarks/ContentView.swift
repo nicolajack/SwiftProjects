d0728edbb0e2139e5100c28e6ac42b1e7bd6b0ff
Nico Jackson <nicolajack@gmail.com>
2025-03-28 10:36:23 -0400

    added framework for Landmarks project

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by nico jackson on 3/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hey nico!")
+            .foregroundColor(Color.purple)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by nico jackson on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
